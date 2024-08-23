4ef15391fae392b4e3fcc00166f8b31a2c13a87a
SvitlanaTesliuk <149165687+SvitlanaTesliuk@users.noreply.github.com>
2024-08-23 18:00:37 +0300

    add hw js 07

---
+++ js/task-6.js
@@ -0,0 +1,44 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const controls = document.getElementById('controls');
+const input = controls.querySelector('input');
+const createBtn = controls.querySelector('[data-create]');
+const destroyBtn = controls.querySelector('[data-destroy]');
+const boxes = document.getElementById('boxes');
+
+function createBoxes(amount) {
+  const elements = [];
+  let size = 30;
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement('div');
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    size += 10;
+    elements.push(div);
+  }
+
+  boxes.append(...elements);
+}
+
+function destroyBoxes() {
+  boxes.innerHTML = '';
+}
+
+createBtn.addEventListener('click', () => {
+  const amount = parseInt(input.value.trim(), 10);
+
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = '';
+  } else {
+    alert('Please enter a number between 1 and 100');
+  }
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
